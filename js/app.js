a68a7187b0ec73972541caa02f1358ebffc9fa44
Rafael Garcia Gabriel <rafaelgarciagabriel@MacBook-Air-de-Rafael.local>
2026-09-12 11:31:05 -0600

    Strictly hide all non-folder items in the default grid view

--- js/app.js
+++ js/app.js
@@ -169253,7 +169253,7 @@ if (AppState.currentMode === 'preorder-sports') currentProducts = SPORTS_PRODUCT
 
 
   const filtered = currentProducts.filter(prod => {
-    if (AppState.currentFilter === 'all' && prod.isProductInsideFolder) return false;
+    if (AppState.currentFilter === 'all' && !prod.isFolder && !prod.category.endsWith('_folder')) return false;
     const matchesCategory = AppState.currentFilter === 'all' || 
                             prod.category === AppState.currentFilter || 
                             prod.brand === AppState.currentFilter;
